5f126a590f04fdfa5d5539e0a64f9d3591684c82
Muhammed Hidash KT <hibahidash84@gmail.com>
2025-11-28 20:47:49 +0530

    after adding two model class for database

--- movie_manager/movies/models.py
+++ movie_manager/movies/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class MovieInfo(models.Model):
+    title=models.CharField(max_length=90)
+    year=models.IntegerField(null=True)
+    description=models.TextField()
+
+class Directors(models.Model):
+    name=models.CharField(max_length=250)
